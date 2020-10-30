2b3283c722d7b328a807201118b1694624ac87cf
瑞希 岩井 <mizuki724724@gmail.com>
2020-10-30 17:11:58 +0900

    TitleSceneの追加

---
+++ ES Game Library/ES Game Library/MyClass/Scenes/Title/TitleScene.cpp
@@ -0,0 +1,64 @@
+#include "../StdAfx.h"
+#include "TitleScene.hpp"
+
+/// <summary>
+/// Allows the game to perform any initialization it needs to before starting to run.
+/// This is where it can query for any required services and load any non-graphic
+/// related content.  Calling base.Initialize will enumerate through any components
+/// and initialize them as well.
+/// </summary>
+bool TitleScene::Initialize()
+{
+	// TODO: Add your initialization logic here
+	Title = GraphicsDevice.CreateSpriteFromFile(_T("TitleSprite/Title.png"));
+
+	return true;
+}
+
+/// <summary>
+/// Finalize will be called once per game and is the place to release
+/// all resource.
+/// </summary>
+void TitleScene::Finalize()
+{
+	// TODO: Add your finalization logic here
+
+}
+
+/// <summary>
+/// Allows the game to run logic such as updating the world,
+/// checking for collisions, gathering input, and playing audio.
+/// </summary>
+/// <returns>
+/// Scene continued value.
+/// </returns>
+int TitleScene::Update()
+{
+    // TODO: Add your update logic here
+	KeyboardBuffer keybuf = Keyboard->GetBuffer();
+	if (keybuf.IsPressed(Keys_Space))
+	{
+		return GAME_SCENE(new GameMain);
+	}
+
+	return 0;
+}
+
+/// <summary>
+/// This is called when the game should draw itself.
+/// </summary>
+void TitleScene::Draw()
+{
+	// TODO: Add your drawing code here
+	GraphicsDevice.Clear(Color_CornflowerBlue);
+
+	GraphicsDevice.BeginScene();
+
+	SpriteBatch.Begin();
+
+	SpriteBatch.Draw(*Title, Vector3(0, 0, 0));
+
+	SpriteBatch.End();
+
+	GraphicsDevice.EndScene();
+}
