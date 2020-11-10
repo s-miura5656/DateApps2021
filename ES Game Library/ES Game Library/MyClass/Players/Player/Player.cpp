de70f2e420fdc42b3356eab562f1c7c6d8e261e0
kawamura <nabsvxzw@gmail.com>
2020-11-10 17:18:06 +0900

    no message

--- ES Game Library/ES Game Library/MyClass/Players/Player/Player.cpp
+++ ES Game Library/ES Game Library/MyClass/Players/Player/Player.cpp
@@ -14,7 +14,6 @@ Player::~Player()
 bool Player::FileInitialize(LPCTSTR& file)
 {
 	player = GraphicsDevice.CreateModelFromFile(file);
-	//rocket_punch = GraphicsDevice.CreateModelFromFile(_T("Player/sword_01.X"));
 	return true;
 }
 
@@ -28,15 +27,6 @@ bool Player::Initialize()
 	arm.Fileinitialize();
 	arm.Initialize();
 
-
-	/*punch_state = NO_PUNCH;
-
-	punch_speed = 0.1f;*/
-
-
-	//rocket_punch->SetScale(0.01f);
-
-	//rocket_punch->SetScale(scale);
 
 	// @brief プレイヤーの当たり判定用の箱
 	//player_obb = player->GetOBB();
@@ -59,11 +49,6 @@ int Player::Update()
 	player_get_pos = player->GetPosition();
 	player_get_rot = player->GetRotation();
 
-	//punch_get_pos = rocket_punch->GetPosition();
-	//punch_get_rot = rocket_punch->GetRotation();
-
-	//float dist = Vector3_Distance(player_get_pos, punch_get_pos);
-
 
 	arm.Update(player_get_pos,angle);
 
@@ -72,29 +57,6 @@ int Player::Update()
 	{
 		arm.ArmShoot(PUNCH);
 	}
-
-	/*if (punch_state == RETURN_PUNCH)
-	{
-		rocket_punch->Move(0, 0, -punch_speed);
-		if (dist <= 0.5) {
-			punch_state = NO_PUNCH;
-			dist = 0;
-		}
-	}
-	else if (punch_state == PUNCH)
-	{
-		rocket_punch->Move(0, 0, punch_speed);
-		if (dist >= 5)
-		{
-			dist = 5;
-			punch_state = RETURN_PUNCH;
-		}
-	}
-	else if (punch_state == NO_PUNCH)
-	{
-		rocket_punch->SetPosition(player_get_pos);
-	}*/
-
 
 	//プレイヤー移動
 	if (pad_state.X != Axis_Center && arm.GetArmState() == NO_PUNCH ||
@@ -107,8 +69,6 @@ int Player::Update()
 		player->Move(0, 0, move_speed);
 
 		arm.SetPra(player_get_pos,angle);
-		/*rocket_punch->SetRotation(0, angle, 0);
-		rocket_punch->SetPosition(player_get_pos);*/
 	}
 
 
