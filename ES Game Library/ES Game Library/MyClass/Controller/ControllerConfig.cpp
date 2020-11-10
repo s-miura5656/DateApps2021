12579231747d3a522132105c040787e3402cca3b
瑞希 岩井 <mizuki724724@gmail.com>
2020-11-10 15:48:07 +0900

    ゲームパッドとキーボードの設定をクラス化をしました。

---
+++ ES Game Library/ES Game Library/MyClass/Controller/ControllerConfig.cpp
@@ -0,0 +1,12 @@
+#include"ControllerConfig.h"
+
+::KeyboardBuffer ControllerConfig::keyboardBuffer;
+
+
+bool ControllerConfig::Initialize()
+{
+	return true;
+}
+void  ControllerConfig::Update()
+{
+}
---
+++ ES Game Library/ES Game Library/MyClass/Controller/ControllerConfig.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include"../ESGLib.h"
+
+class ControllerConfig
+{
+public:
+	ControllerConfig(int number)
+	{
+		pad_number = number;
+	}
+
+	~ControllerConfig() {}
+
+	bool Initialize();
+	void Update();
+
+	GamePadBuffer PadBuffer() { return GamePad(pad_number)->GetBuffer(); }
+
+	bool GetButtonBuffer(int button_num) {
+		return PadBuffer().IsPressed(button_num);
+	}
+
+	GamePadState PadState() { return GamePad(pad_number)->GetState(); }
+
+	bool GetButtonState(int button_num) {
+		return PadState().Buttons[button_num - GamePad_Button1];
+	}
+
+	float GetPadStateX() { return PadState().X / move_value_constant; }
+
+	float GetPadStateY() { return PadState().Y / move_value_constant; }
+
+	KeyboardState KeyboardState() { return Keyboard->GetState(); }
+
+	bool KeyboardButtonState(int button_num) {
+		return KeyboardState().IsKeyDown(button_num);
+	}
+
+	bool KeyboardButtonBuffer(int button_num) {
+		return keyboardBuffer.IsPressed(button_num);
+	}
+
+	static void GetKeyboardBuffer()
+	{
+		keyboardBuffer = Keyboard->GetBuffer();
+	}
+
+private:
+	int pad_number;
+	int const move_value_constant = 1000;
+
+	static KeyboardBuffer keyboardBuffer;
+};
