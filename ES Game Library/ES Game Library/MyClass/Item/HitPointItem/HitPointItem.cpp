de2098f4ea1d47b8bdd3d8955fea9665bce7056c
lan16wink@gmail.com <lan16wink@gmail.com>
2020-11-12 18:07:40 +0900

    アイテム各種を取得した際消えるように更新

--- ES Game Library/ES Game Library/MyClass/Item/HitPointItem/HitPointItem.cpp
+++ ES Game Library/ES Game Library/MyClass/Item/HitPointItem/HitPointItem.cpp
@@ -1,4 +1,21 @@
 #include "HitPointItem.h"
+#include "../../Data/GameData.h"
+
+HitPointItem::HitPointItem(Vector3 position, std::string name)
+{
+	this->_position = position;
+	_hit_box.reset(new HitBox());
+	_hit_box->Init();
+	_hit_box->Settags(name);
+	_hit_box->SetHitBox(1, 1, 1);
+	_iplayer_data = new IPrayerData;
+}
+
+HitPointItem::~HitPointItem()
+{
+	delete _iplayer_data;
+}
+
 
 bool HitPointItem::Initialize()
 {
@@ -17,5 +34,18 @@ bool HitPointItem::Initialize()
 
 int HitPointItem::Update()
 {
+	for (int i = 0; i < PLAYER_COUNT_MAX; i++)
+	{
+		std::string name = PLAYER_TAG + std::to_string(i + 1);
+
+		if (_hit_box->IsHitObjects(name))
+		{
+			ItemEffect(name);
+			return END;
+		}
+	}
+
+	Removeflag = true;
+
 	return 0;
 }
