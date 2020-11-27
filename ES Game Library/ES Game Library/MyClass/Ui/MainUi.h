54d8f5780ad81483ac4d1d657310997b63988919
r9y8u1i@yahoo.co.jp <r9y8u1i@yahoo.co.jp>
2020-11-27 14:53:28 +0900

    HPゲージの減少と赤ゲージの減少のHpAnimation関数の作成。 ダメージを受けたかわかるSetDamege関数の作成。

--- ES Game Library/ES Game Library/MyClass/Ui/MainUi.h
+++ ES Game Library/ES Game Library/MyClass/Ui/MainUi.h
@@ -16,17 +16,22 @@ public:
 	int Update() override { return 0; }
 	void Draw2D() override;
 	void DebugDraw();
+	void HpAnimation();
+	void SetDamege(const int player_number, const int damage);
 
 	FONT player_date;//各プレイヤーのステータス表記
 	Vector2 powor_ui_pos[PLAYER_COUNT_MAX];//各プレイヤーのパワー
-	Vector2 hp_ui_pos[PLAYER_COUNT_MAX];//各プレイヤーの㏋
+	Vector3 hp_ui_pos[PLAYER_COUNT_MAX];//各プレイヤーの㏋
 	Vector2 speed_ui_pos[PLAYER_COUNT_MAX];//各プレイヤーのスピード
 
 	Color color[PLAYER_COUNT_MAX];//各プレイヤーの色指定
 
-	SPRITE hp;
-	RectWH hp_;
-	float hp_x, hp_y;
-
+	//HPアニメーション
+	SPRITE HP_green;
+	SPRITE HP_red;
+	SPRITE HP_black;
+	
+	float green_x[PLAYER_COUNT_MAX];
+	float red_x[PLAYER_COUNT_MAX];
 
 };
