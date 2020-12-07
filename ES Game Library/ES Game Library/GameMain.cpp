44faefbd89a2a7a2e35dbe1793b43d2ec36e74b5
souta_miura <miura042910@gmail.com>
2020-12-07 21:17:03 +0900

    no message

--- ES Game Library/ES Game Library/GameMain.cpp
+++ ES Game Library/ES Game Library/GameMain.cpp
@@ -51,7 +51,7 @@ int GameMain::Update()
 void GameMain::Draw()
 {
 	// TODO: Add your drawing code here
-	GraphicsDevice.Clear(Color_CornflowerBlue);
+	GraphicsDevice.Clear(Color_Black);
 
 	GraphicsDevice.BeginScene();
 
