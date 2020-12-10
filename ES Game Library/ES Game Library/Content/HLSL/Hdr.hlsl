27f791b5e4602946afb72a6b44205a76d361e638
souta_miura <miura042910@gmail.com>
2020-12-11 01:39:46 +0900

    no message

--- ES Game Library/ES Game Library/Content/HLSL/Hdr.hlsl
+++ ES Game Library/ES Game Library/Content/HLSL/Hdr.hlsl
@@ -5,8 +5,7 @@ float4 PS_P0_Main(float2 UV : TEXCOORD0) : COLOR0
 {
     float4 color = tex2D(tex0, UV);
     color *= exp2(exposure);
-    color = max(color - 0.004, 0.0);
-    color = (color * (6.2 * color + 0.5)) / (color * (6.2 * color + 1.7) + 0.06);
+    color /= 1.6f + color;
 
     return color;
 }
