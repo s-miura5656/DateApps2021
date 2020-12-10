5fa708fb110e2387016e7d3bc6815784365bf71c
r9y8u1i@yahoo.co.jp <r9y8u1i@yahoo.co.jp>
2020-12-10 11:11:58 +0900

    スコア表記を画像ファイルで行いました。座標も合わせました。

--- ES Game Library/ES Game Library/MyClass/Ui/MainUi.cpp
+++ ES Game Library/ES Game Library/MyClass/Ui/MainUi.cpp
@@ -25,7 +25,7 @@ bool MainUi::Initialize()
 	green_banner    = GraphicsDevice.CreateSpriteFromFile(_T("BannerFrameSprite/green2.png"));
 	yellow_banner   = GraphicsDevice.CreateSpriteFromFile(_T("BannerFrameSprite/yellow2.png"));
 	time_banner     = GraphicsDevice.CreateSpriteFromFile(_T("BannerFrameSprite/time_2.png"));
-	score_num       = GraphicsDevice.CreateSpriteFromFile(_T("NumberSprite/number_3.png"));
+	score_num       = GraphicsDevice.CreateSpriteFromFile(_T("NumberSprite/namber.png"));
 
 	banner_pos[0] = Vector3(80  , 30, 1);
 	banner_pos[1] = Vector3(300 , 30, 1);
@@ -44,10 +44,10 @@ bool MainUi::Initialize()
 	speed_ui_pos[2] = Vector2(  10, 680);
 	speed_ui_pos[3] = Vector2(1025, 680);
 
-	color[0] = Color(255,   0, 0  );
-	color[1] = Color(0  ,   0, 255);
-	color[2] = Color(0  , 255, 0  );
-	color[3] = Color(255, 255,  0 );
+	color[0] = Color(255,   0,   0);
+	color[1] = Color(  0,   0, 255);
+	color[2] = Color(  0, 255,   0);
+	color[3] = Color(255, 255,   0);
 
 	InputDevice.CreateKeyboard();
 
@@ -101,42 +101,22 @@ void MainUi::PlayerBanner()
 	for (int i = 0; i < PLAYER_COUNT_MAX; i++)
 	{
 		std::string& tag = PLAYER_TAG + std::to_string(i + 1);
-	
-		
+
+
 		//int score = param_list[tag].ranking_point;
 		int score = iplayer_data->GetRankingPoint(tag);
 
-		SpriteBatch.DrawString(player_date, score_pos[i], color[i], _T("%dp_Point : %d"), i+1, iplayer_data->GetRankingPoint(tag));
+		SpriteBatch.DrawString(player_date, score_pos[i], color[i], _T("%dp_Point : "), i + 1, iplayer_data->GetRankingPoint(tag));
 		SpriteBatch.Draw(*red_banner, banner_pos[0]);
 		SpriteBatch.Draw(*blue_banner, banner_pos[1]);
 		SpriteBatch.Draw(*green_banner, banner_pos[2]);
 		SpriteBatch.Draw(*yellow_banner, banner_pos[3]);
 
-
 		//スコアアニメーション
-		if (score > 999)
-		{
-			SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3(100, 0, -1), RectWH((int)(score / 1000) * 64  , 0, 64, 64));
-			SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3(200, 0, -1), RectWH((int)((score % 1000) / 100) * 64, 0, 64, 64));
-			SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3(300, 0, -1), RectWH((int)((score % 1000) % 100 / 10) * 64, 0, 64, 64));
-			SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3(400, 0, -1), RectWH((int)((score % 1000) % 100 % 10) * 64, 0, 64, 64));
-		}
-		else if (score > 99)
-		{
-			SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3(100, 0, -1), RectWH((int)(score / 100) * 64, 0, 100, 64));
-			SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3(200, 0, -1), RectWH((int)((score % 100) / 10) * 64, 0, 64, 64));
-			SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3(300, 0, -1), RectWH((int)((score % 100) % 10) * 64, 0, 64, 64));
-		}
-		else if (score > 9)
-		{
-			SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3(100, 0, -1), RectWH((int)(score / 10) * 64, 0, 64, 64));
-			SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3(200, 0, -1), RectWH((int)(score % 10) * 64, 0, 64, 64));
-		}
-		else
-		{
-			SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3(100, 0, -1), RectWH((int)(score) * 64, 0, 64, 64));
-		}
-
+		SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3( (10 * 0.3) + 100, 10, -1), RectWH((int)(score / 1000) * 64, 0, 64, 64), (DWORD)Color_White, Vector3(0, 0, 0),Vector3(0,0,0),0.3f);
+		SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3( (74 * 0.3) + 100, 10, -1), RectWH((int)((score % 1000) / 100) * 64, 0, 64, 64),(DWORD)Color_White, Vector3(0, 0, 0), Vector3(0, 0, 0), 0.3f);
+		SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3( (138 * 0.3) + 100, 10, -1), RectWH((int)((score % 1000) % 100 / 10) * 64, 0, 64, 64), (DWORD)Color_White, Vector3(0, 0, 0), Vector3(0, 0, 0), 0.3f);
+		SpriteBatch.Draw(*score_num, banner_pos[i] + Vector3( (204 * 0.3) + 100, 10, -1), RectWH((int)((score % 1000) % 100 % 10) * 64, 0, 64, 64), (DWORD)Color_White, Vector3(0, 0, 0), Vector3(0, 0, 0), 0.3f);
 	}
 }
 
