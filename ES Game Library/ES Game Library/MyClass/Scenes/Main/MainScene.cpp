3a599d96b742ecc9fa68ee857977910a2da11bbd
瑞希 岩井 <mizuki724724@gmail.com>
2020-12-16 14:36:53 +0900

    コード整理

--- ES Game Library/ES Game Library/MyClass/Scenes/Main/MainScene.cpp
+++ ES Game Library/ES Game Library/MyClass/Scenes/Main/MainScene.cpp
@@ -55,48 +55,7 @@ int MainScene::Update()
 	float timeleft = TimeManager::Instance().GetTimeLeft();
 	if (timeleft <= 0.9f)
 	{
-		// ここでプレイヤーマネージャーから各プレイヤーのポイントを取得
-		int points[PLAYER_COUNT_MAX] = {};
-		int ranknum[PLAYER_COUNT_MAX] = {};
-		IPrayerData* pPlayerData = new IPrayerData;
-		for (int i = 0; i < PLAYER_COUNT_MAX; i++)
-		{
-			std::string tag = PLAYER_TAG + std::to_string(i + 1);
-			if (pPlayerData->GetRankNum(tag) == 0) {
-				points[0] = pPlayerData->GetRankingPoint(tag);
-				ranknum[0] = i + 1;
-				break;
-			}
-		}
-		for (int i = 0; i < PLAYER_COUNT_MAX; i++)
-		{
-			std::string tag = PLAYER_TAG + std::to_string(i + 1);
-			if (pPlayerData->GetRankNum(tag) == 1) {
-				points[1] = pPlayerData->GetRankingPoint(tag);
-				ranknum[1] = i + 1;
-				break;
-			}
-		}
-		for (int i = 0; i < PLAYER_COUNT_MAX; i++)
-		{
-			std::string tag = PLAYER_TAG + std::to_string(i + 1);
-			if (pPlayerData->GetRankNum(tag) == 2) {
-				points[2] = pPlayerData->GetRankingPoint(tag);
-				ranknum[2] = i + 1;
-				break;
-			}
-		}
-		for (int i = 0; i < PLAYER_COUNT_MAX; i++)
-		{
-			std::string tag = PLAYER_TAG + std::to_string(i + 1);
-			if (pPlayerData->GetRankNum(tag) == 3) {
-				points[3] = pPlayerData->GetRankingPoint(tag);
-				ranknum[3] = i + 1;
-				break;
-			}
-		}
-		SceneManager::Instance().SetResultData(ranknum, points);	// ここで誰が一位だったかをPlayerタグで判定するため、引数1はstring,引数2はintのポインタ		
-		SceneManager::Instance().ChangeScene(SceneManager::Instance().RESULT);
+		ChangeSceneResult();
 	}
 
 	return 0;
@@ -122,4 +81,41 @@ void MainScene::Draw3D()
 	}
 
 	Effekseer.Draw();
+}
+void MainScene::ChangeSceneResult()
+{
+	// ここでプレイヤーマネージャーから各プレイヤーのポイントを取得
+	int points[PLAYER_COUNT_MAX] = {};
+	int ranknum[PLAYER_COUNT_MAX] = {};
+	IPrayerData* pPlayerData = new IPrayerData;
+	for (int i = 0; i < PLAYER_COUNT_MAX; i++)
+	{
+		std::string tag = PLAYER_TAG + std::to_string(i + 1);
+		points[i] = pPlayerData->GetRankingPoint(tag);
+		ranknum[i] = i + 1;
+	}
+	int temp;
+	for (int i = 0; i < PLAYER_COUNT_MAX; i++)
+	{
+		for (int j = PLAYER_COUNT_MAX - 1; j > i; j--)
+		{
+			if (points[j - 1] < points[j])
+			{
+				temp = points[j - 1];
+				points[j - 1] = points[j];
+				points[j] = temp;
+			}
+		}
+		for (int n = PLAYER_COUNT_MAX - 1; n > i; n--)
+		{
+			if (ranknum[n - 1] > ranknum[n])
+			{
+				temp = ranknum[n - 1];
+				ranknum[n - 1] = ranknum[n];
+				ranknum[n] = temp;
+			}
+		}
+	}
+	SceneManager::Instance().SetResultData(ranknum, points);//プレイヤー名とポイント数を一位から昇順に並べ、代入する。
+	SceneManager::Instance().ChangeScene(SceneManager::Instance().RESULT);
 }
--- ES Game Library/ES Game Library/MyClass/Scenes/Main/MainScene.h
+++ ES Game Library/ES Game Library/MyClass/Scenes/Main/MainScene.h
@@ -17,7 +17,7 @@ public:
 	int Update();
 	void Draw2D();
 	void Draw3D();
-
+	void ChangeSceneResult();
 private:
 
 	std::vector<ManagerBase*> _managers;
