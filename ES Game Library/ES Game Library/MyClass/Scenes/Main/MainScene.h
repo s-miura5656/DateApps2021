d10e10c77932ba55700f87b026d61e16b2e1c580
瑞希 岩井 <mizuki724724@gmail.com>
2020-12-18 17:52:10 +0900

    リザルトが表示されなかったので表示されるようにしました。変数名の変更とコード整理をしました。

--- ES Game Library/ES Game Library/MyClass/Scenes/Main/MainScene.h
+++ ES Game Library/ES Game Library/MyClass/Scenes/Main/MainScene.h
@@ -13,19 +13,11 @@ public:
 	MainScene();
 	~MainScene();
 
-<<<<<<< HEAD
 	bool Initialize() override;
 	int Update() override;
 	void Draw2D() override;
 	void Draw3D() override;
 	void DrawAlpha3D() override;
-=======
-	bool Initialize();
-	int Update();
-	void Draw2D();
-	void Draw3D();
-	void ChangeSceneResult();
->>>>>>> cf893e710f413e5dde5084f45cb4a2ee7ff9259e
 private:
 	void ResultTransition();
 	std::vector<ManagerBase*> _managers;
