3e1d684a259baa6443eee586d60718e2db43bc4d
瑞希 岩井 <mizuki724724@gmail.com>
2021-01-26 13:22:52 +0900

    リザルトのオーディオバグ改善、スコアをメインで使っている画像に変更

--- ES Game Library/ES Game Library/MyClass/Scenes/Result/ResultScene.cpp
+++ ES Game Library/ES Game Library/MyClass/Scenes/Result/ResultScene.cpp
@@ -13,7 +13,6 @@ ResultScene::ResultScene()
 
 ResultScene::~ResultScene()
 {
-	AudioManager::Instance().ResultBgmStop();
 	SceneManager::Instance().ClearPlayerTexture();
 }
 
@@ -32,12 +31,12 @@ bool ResultScene::Initialize()
 	_robot_fece          = ResouceManager::Instance().LordSpriteFile(_T("ResultSprite/result_robo.png"));
 	_light               = ResouceManager::Instance().LordSpriteFile(_T("ResultSprite/light.png"));
 	_totitle	         = ResouceManager::Instance().LordSpriteFile(_T("ResultSprite/button.png"));
-	_font                = ResouceManager::Instance().LordFontFile(_T("チェックアンドU-Foフォント"), 50);
 	_shader  	         = ResouceManager::Instance().LordEffectFile(_T("HLSL/AnimationStandardShader.hlsl"));
 	_player_model        = ResouceManager::Instance().LoadAnimationModelFile(_T("Player/Robo_animation.X"));
-
+	_score_sprite        = ResouceManager::Instance().LordSpriteFile(_T("NumberSprite/namber.png"));
 	//!ロボUI初期設定
 	std::string first_name = PLAYER_TAG + to_string(GetRankNum(0));
+
 	std::string player_number = PLAYER_TAG + to_string(SceneManager::Instance().GetPlayerTexture(first_name));
 	auto path = ConvertFilePath("Player/", player_number, ".png");
 	_texture = ResouceManager::Instance().LordSpriteFile(path.c_str());
@@ -72,38 +71,8 @@ bool ResultScene::Initialize()
 	_player_model->RegisterBoneMatricesByName(_shader, "WorldMatrixArray", "NumBones");
 	_player_model->SetTrackEnable(5, TRUE);
 
-	//!プレイヤーモデルのスケールの設定
-	_player_model_big_scale   = 1.0;
-	_player_model_small_scale = 0.5;
-
-	//!プレイヤーのX座標の位置ずれ修正
-	_player_big_position_x   = 0.8;
-	_player_small_position_x = 1.5;
-
-	//!ポイントの順位のX座標の位置ずれ修正
-	_point_small_text_position_correction = Vector2 (420,25);
-	_point_big_text_position_correction = Vector2(420, 25);
-
-	//!ポイントのテキストのスケールの設定
-	_text_big_size   = Vector2(1.0, 1.0);
-	_text_small_size = Vector2(0.5, 0.5);
-
-	//!プレイヤーの順位の座標の設定
-	_player_big_rank_number_position = Vector2(0,-50);
-	_player_small_rank_number_position = Vector2(0, -30);
-
-	//!ポイントの順位の座標の設定
-	_point_big_text_position       = Vector2(430, 430);
-	_point_small_text_position     = Vector2(220, 400);
-
-	//!プレイヤーの座標の設定
-	_player_big_position   = Vector3(-0.6, 0, -7.5);
-	_player_small_position = Vector3(-3.0, 0.2, -7.5);
-
 	//背景の座標
 	_background_position = Vector3(0, 0, 10000);
-	//シーン遷移の方法の座標
-	_totitle_position    = Vector3(900, 640, 0);
 
 	return true;
 }
@@ -120,6 +89,9 @@ int ResultScene::Update()
 		pad->Refresh();
 		if (pad->ButtonDown(BUTTON_INFO::BUTTON_A))
 		{
+			AudioManager::Instance().ResultBgmStop();
+			_player_model->SetTrackPosition(5, 0);
+			_player_model->SetTrackEnable(5, false);
 			SceneManager::Instance().SetSceneNumber(SceneManager::SceneState::TITLE);
 		}
 	}
@@ -134,13 +106,26 @@ void ResultScene::Draw2D()
 	SpriteBatch.Draw(*_background,_background_position);
 
 	SpriteBatch.Draw(*_light, Vector3(0, 0, 10000));
-	//SpriteBatch.Draw(*_robot_win, Vector3(45, 105, 10000), RectWH((GetRankNum(0) - 1) * 512, 0, 512, 512));
-	SpriteBatch.DrawString(_font, Vector2(200, 600), Color(255, 255, 255), _T("%dpt"), GetPoints(0));
 
-	for (int i = 0; i < PLAYER_COUNT_MAX - 1; i++)
+	SpriteBatch.Draw(*_score_sprite, Vector3(150,600,0), RectWH((int)(GetRankNum(0) / 1000) * 64, 0, 64, 64),
+		(DWORD)Color_White, Vector3_Zero, Vector3(32, 32, 0), 1.5f);
+
+	for (int i = 1; i < 4; i++)
 	{
-		SpriteBatch.Draw(*_robot_fece, Vector3(850, 120 + 205 * i, 10000),RectWH(_player_rectwh[i],0, 128,128));
-		SpriteBatch.DrawString(_font, Vector2(980, 170 + 210 * (i)), Color(255, 0, 0), _T("%dpt"), GetPoints(i + 1));
+		SpriteBatch.Draw(*_score_sprite, Vector3(96 * i + 150, 600, 0), RectWH((int)((GetRankNum(0) % 1000) / 100) * 64, 0, 64, 64),
+			(DWORD)Color_White,Vector3_Zero, Vector3(32,32,0),1.5f);
+	}
+
+	for (int i = 0; i < PLAYER_COUNT_MAX; i++)
+	{
+		SpriteBatch.Draw(*_robot_fece, Vector3(850, 120 + 205 * i, 10000), RectWH(_player_rectwh[i], 0, 128, 128));
+		SpriteBatch.Draw(*_score_sprite, Vector3(980, 175 + (210 * i), 0), RectWH((int)(GetRankNum(i) / 1000) * 64, 0, 64, 64),
+			(DWORD)Color_White, Vector3_Zero, Vector3(32, 32, 0), 0.5f);
+		for (int j = 1; j < PLAYER_COUNT_MAX; j++)
+		{
+			SpriteBatch.Draw(*_score_sprite, Vector3(980 + 32 * j, 175 + (210 * i), 0), RectWH((int)((GetRankNum(i) % 1000) / 100) * 64, 0, 64, 64),
+				(DWORD)Color_White, Vector3_Zero, Vector3(32, 32, 0), 0.5f);
+		}
 	}
 }
 /*
@@ -151,7 +136,7 @@ void ResultScene::Draw3D()
 	Matrix vp = SceneCamera::Instance().GetCamera()->GetViewProjectionMatrix();
 	SceneCamera::Instance().SetSceneCamera();
 
-	_shader->SetTexture("m_Texture", * _texture);
+	_shader->SetTexture("m_Texture", *_texture);
 	_shader->SetParameter("vp", vp);
 	_shader->SetTechnique("UnlitAnimationModel");
 	_player_model->SetTrackPosition(5, _animation_count);
@@ -196,111 +181,4 @@ int ResultScene::GetPoints	(int player_num)
 {
 	auto resultdata = SceneManager::Instance().GetResultData();
 	return resultdata->points[player_num];
-}
-
-/**
- * @brief　プレイヤーモデルのスケールを設定する
- * @param (player_num) 順位が何番目のプレイヤーか
- * @return プレイヤーのスケールにセットするfloat型の値で返す
- */
-float ResultScene::SetPlayerScale(int player_num)
-{
-	float pl_model_scale;
-
-	if (player_num < _arrival_count) {
-		pl_model_scale = _player_model_big_scale;
-	}
-	else
-	{
-		pl_model_scale = _player_model_small_scale;
-	}
-
-	return pl_model_scale;
-}
-
-/**
- * @brief　テキストのサイズを設定する
- * @param (player_num) 順位が何番目のプレイヤーか
- * @return テキストのサイズをVector2型の値で返す
- */
-Vector2 ResultScene::SetTextSize(int player_num)
-{
-	Vector2 text_size;
-	
-	if (player_num < _arrival_count) {
-		text_size = Vector2(_text_big_size);
-	}
-	else
-	{
-		text_size = Vector2(_text_small_size);
-	}
-
-	return text_size;
-}
-
-/**
- * @brief　プレイヤーの座標を設定する
- * @param (player_num) 順位が何番目のプレイヤーか
- * @return プレイヤーの座標をVector3型の値で返す
- */
-Vector3 ResultScene::SetPlayerPosition(int player_num)
-{
-	Vector3 pl_pos;
-
-	if (player_num < _arrival_count) {
-		pl_pos    = _player_big_position;
-		pl_pos.x += _player_big_position_x * player_num;
-	}
-	else
-	{
-		pl_pos    = _player_small_position;
-		pl_pos.x += _player_small_position_x * player_num;
-		pl_pos.y += -0.2 * player_num;
-	}
-
-	return pl_pos;
-}
-
-/**
- * @brief　プレイヤーの座標を設定する
- * @param (player_num) 順位が何番目のプレイヤーか
- * @return プレイヤーの順位の座標をVector3型の値で返す
- */
-Vector3 ResultScene::SetPlayerRankNumberPosition(int player_num)
-{
-	Vector3 pl_rank_pos = (Vector3_Zero);
-
-	if (player_num < _arrival_count) 
-	{
-		pl_rank_pos.x = SetPointTextPosition(player_num).x;
-		pl_rank_pos.y = SetPointTextPosition(player_num).y + _player_big_rank_number_position.y;
-	}
-	else
-	{
-		pl_rank_pos.x = SetPointTextPosition(player_num).x;
-		pl_rank_pos.y = SetPointTextPosition(player_num).y + _player_small_rank_number_position.y;
-	}
-
-	return pl_rank_pos;
-}
-
-/**
- * @brief　ポイントの座標を設定する
- * @param (player_num) 順位が何番目のプレイヤーか
- * @return ポイントの座標をVector2型の値で返す
- */
-Vector2 ResultScene::SetPointTextPosition(int player_num)
-{
-	Vector2 pointpos;
-
-	if (player_num < _arrival_count) {
-		pointpos    = _point_big_text_position;
-	}
-	else {
-		pointpos    = _point_small_text_position;
-		pointpos += Vector2(_point_small_text_position_correction.x * (player_num - _arrival_count), 
-			                _point_small_text_position_correction.y * (player_num - _arrival_count + 1));
-	}
-
-	return pointpos;
 }
